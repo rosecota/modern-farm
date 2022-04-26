c14a2e0a75f310f90b2d21551ac46906ae1b951d
Rose Cota <rosejcota@gmail.com>
2022-04-26 16:20:53 -0500

    Update harverster functionality to account for corn output being half so tests pass

--- src/scripts/harvester.js
+++ src/scripts/harvester.js
@@ -11,6 +11,10 @@ export const harvestPlants = (plants) => {
 };
 
 const repeatPlant = (item, count) => {
+  // Again, the exception is corn. Half of your corn will be sold to cattle ranchers, so only half of the output of each corn plant will be added to the array that this function returns.
+  if (item.type === "Corn") {
+    count = count / 2;
+  }
   // Add that many of the plant objects to the array that the function returns.
   return Array(count).fill(item);
 };
